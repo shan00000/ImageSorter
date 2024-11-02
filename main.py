5848371eb06eaaa9141569f4201c909208041546
shanr <shanrathnayak15@gmail.com>
2024-11-02 23:10:30 +0000

    made few changes

--- main.py
+++ main.py
@@ -1,70 +1,56 @@
 import os
 from inputfile import check_dir
 
+duplicates_found = 0
 
 ## currently working on it.
-def save_duplicates(file, root, filename):
-    duplicate_file_path = os.path.join(root, "duplicate")
-    destination = os.path.join(duplicate_file_path,filename)
-    count = 0
-
-    while os.path.isfile(destination):
-        path, extension = str(destination).split(".")
-        path = path + f"({count})"
-        destination = path + "." + extension
-        count += 1
+def newFilePath(file_name, duplicate_folder_path):
+    global duplicates_found
+    duplicates_found = duplicates_found + 1
+    file_name, extension = file_name.split('.')
+    new_file_name = file_name + "-" + f"{str(duplicates_found)}." + extension
+    print(new_file_name)
+    return os.path.join(duplicate_folder_path, new_file_name)
 
 
-    if check_dir(duplicate_file_path):
-        os.replace(file, destination)
+def saveDuplicates(file_path, target_dir, file_name):
+    duplicate_folder_path = os.path.join(target_dir, "PossibleDuplicate")
+    new_file_path = newFilePath(file_name, duplicate_folder_path)
+    if check_dir(duplicate_folder_path):
+        os.replace(file_path, new_file_path)
+        print("Duplicate")
     else:
-        os.makedirs(duplicate_file_path)
-        os.replace(file, destination)
+        os.makedirs(duplicate_folder_path)
+        os.replace(file_path, new_file_path)
+
 
 
 ## somewhat done
-def flatten_files(source_dir, target_dir=None, is_sub_dir=False):
+def flattenFiles(source_dir, target_dir=None, is_sub_dir=False):
     if target_dir is None: target_dir = source_dir
     items_in_dir = os.listdir(source_dir)
     for item in items_in_dir:
         item_path = os.path.join(source_dir, item)
         if os.path.isdir(item_path):
-            flatten_files(item_path, target_dir, True)
+            flattenFiles(item_path, target_dir, True)
         else:
             if is_sub_dir:
-                savepath = os.path.join(target_dir, item)
-                if os.path.isfile(savepath):
-                    save_duplicates(savepath, target_dir, item)
+                save_path = os.path.join(target_dir, item)
+                if os.path.isfile(save_path):
+                    saveDuplicates(item_path, target_dir, item)
                 else:
-                    os.replace(item_path, savepath)
+                    os.replace(item_path, save_path)
+                    continue
 
 
-## to put into files
-# def getallfilenames(filepaths, diric=None):
-#         for currentdir in filepaths:
-#             filesavailable = os.listdir(currentdir)
-#             for file in filesavailable:
-#                 if os.path.isdir(file):
-#
-#                 else:
-#                     root, extension = os.path.splitext(file)
-#
-#                     curentfilelocation = os.path.join(currentdir, file)
-#                     filetosave = os.path.join(currentdir, extension)
-#                     futurelocation = os.path.join(filetosave,file)
-#
-#                     if check_dir(filetosave):
-#                         os.replace(curentfilelocation,futurelocation)
-#                     else:
-#                         os.makedirs(filetosave)
-#                         os.replace(curentfilelocation, futurelocation)
+
 
 
 def main():
-    filepaths = [r"C:\Users\sr2559g\OneDrive - University of Greenwich\Pictures\Teams Backgrounds"]
     # move_files_to_root(filepaths[0])
     # filesinDir = getallfilenames(filepaths)
-    flatten_files(filepaths[0])
+    flattenFiles(file_paths[0])
+
 
 
 main()
---
+++ testFile.py
@@ -0,0 +1,21 @@
+## to put into files
+# def getallfilenames(filepaths, diric=None):
+#         for currentdir in filepaths:
+#             filesavailable = os.listdir(currentdir)
+#             for file in filesavailable:
+#                 if os.path.isdir(file):
+#
+#                 else:
+#                     root, extension = os.path.splitext(file)
+#
+#                     curentfilelocation = os.path.join(currentdir, file)
+#                     filetosave = os.path.join(currentdir, extension)
+#                     futurelocation = os.path.join(filetosave,file)
+#
+#                     if check_dir(filetosave):
+#                         os.replace(curentfilelocation,futurelocation)
+#                     else:
+#                         os.makedirs(filetosave)
+#                         os.replace(curentfilelocation, futurelocation)
+
+
